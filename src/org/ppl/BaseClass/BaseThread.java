721c827d064801a9e1e225e5a785b1ae9e59a532
tea <vyouzhi@gmail.com>
2015-02-07 18:29:34 +0800

    thread queue

--- src/org/ppl/BaseClass/BaseThread.java
+++ src/org/ppl/BaseClass/BaseThread.java
@@ -7,7 +7,4 @@ public abstract class BaseThread extends PObject {
 	public abstract boolean isRun();
 	public abstract boolean Stop();
 	
-	public void name() {
-		
-	}
 }
--- src/org/ppl/core/ServletApplicationLifeListener.java
+++ src/org/ppl/core/ServletApplicationLifeListener.java
@@ -1,36 +1,58 @@
 package org.ppl.core;
 
+import java.util.List;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import org.ppl.BaseClass.BaseThread;
 import org.ppl.BaseClass.LibLang;
 import org.ppl.Module.ModuleBind;
 import org.ppl.db.HikariConnectionPool;
+import org.ppl.etc.Config;
+import org.ppl.etc.globale_config;
 
+import com.alibaba.fastjson.JSON;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.name.Names;
 
 @WebListener
-public class ServletApplicationLifeListener extends PObject implements ServletContextListener {
+public class ServletApplicationLifeListener extends PObject implements
+		ServletContextListener {
+	private Config mConfig;
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
-		// TODO Auto-generated method stub		
+		// TODO Auto-generated method stub
 		HikariConnectionPool.getInstance();
-		  
-		
+
+		mConfig = new Config(globale_config.Config);
+		int autorun = mConfig.GetInt("autorun");
+		if (autorun == 1) {
+			Auto();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void Auto() {
+		String runJson = mConfig.GetValue("rum.module");
+		List<String> runList = JSON.parseObject(runJson, List.class);
 		Injector injector = Guice.createInjector(new ModuleBind());
-		LibLang libLan = (LibLang) injector.getInstance(Key.get(
-				LibLang.class, Names.named("")));
+		for (String rl : runList) {
+			BaseThread libLan = (BaseThread) injector.getInstance(Key.get(
+					BaseThread.class, Names.named(rl)));
+			libLan.Run();
+		}
+
 	}
 
 }
